cb101469694fcdd7618e536dc4b4fa605b285ca7
Stanislav Grokhotov <grokhotov@isands.ru>
2022-03-29 18:52:27 +0300

    Initial commit

---
+++ src/main/java/com/insideclub/telegrambot/entity/Bot.java
@@ -0,0 +1,14 @@
+package com.insideclub.telegrambot.entity;
+
+
+public class Bot {
+
+    private final String BOT_NAME;
+    private final String BOT_TOKEN;
+
+
+    public Bot(String bot_name, String bot_token) {
+        BOT_NAME = bot_name;
+        BOT_TOKEN = bot_token;
+    }
+}
---
+++ src/test/java/com/insideclub/telegrambot/TelegramBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.insideclub.telegrambot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelegramBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
