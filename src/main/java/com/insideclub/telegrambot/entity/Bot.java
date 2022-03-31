bb737caeb15d012c66d0cc4b1fa4cc6742792c78
Stanislav Grokhotov <grokhotov@isands.ru>
2022-03-31 19:36:50 +0300

    added NonCommand

--- src/main/java/com/insideclub/telegrambot/entity/Bot.java
+++ src/main/java/com/insideclub/telegrambot/entity/Bot.java
@@ -6,9 +6,13 @@ public class Bot {
     private final String BOT_NAME;
     private final String BOT_TOKEN;
 
+    private final NonCommand nonCommand;
 
-    public Bot(String bot_name, String bot_token) {
+    public Bot(String bot_name, String bot_token, NonCommand nonCommand) {
         BOT_NAME = bot_name;
         BOT_TOKEN = bot_token;
+        this.nonCommand = nonCommand;
+
     }
+
 }
---
+++ src/main/java/com/insideclub/telegrambot/entity/NonCommand.java
@@ -0,0 +1,5 @@
+package com.insideclub.telegrambot.entity;
+
+public class NonCommand {
+
+}
